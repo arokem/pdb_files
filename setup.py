061405cdb810fab4fb57cb1be666f6976cff8b6e
arokem <arokem@gmail.com>
2015-01-30 10:50:30 -0800

    NF: CLI.

--- setup.py
+++ setup.py
@@ -11,4 +11,5 @@ from distutils.core import setup
 setup(name='pdb_files',
       packages = ['pdb_files'],
       package_dir = {'pdb_files':'.'},
-      package_data={'pdb_files': ['./*.py']})
+      package_data={'pdb_files': ['./*.py']},
+      scripts=['./bin/pdb2trk', './bin/trk2pdb'])
